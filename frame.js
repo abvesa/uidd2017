4f4652625e927ac68a643d4032e7102d40099dbb
lavaborg <elunivch@gmail.com>
2017-03-20 19:11:03 +0800

    test

--- frame.js
+++ frame.js
@@ -5,11 +5,40 @@ const game = new Phaser.Game(w, h, Phaser.AUTO, 'frame', { preload: preload, cre
 
 
 
+function preload(){
+    game.load.spritesheet('intro1', 'assets/images/intro1.jpg');
+    game.load.spritesheet('intro2', 'assets/images/intro2.jpg');
+    game.load.spritesheet('intro3', 'assets/images/intro3.jpg');
+    game.load.spritesheet('intro4', 'assets/images/intro4.jpg');
+    game.load.spritesheet('intro_up', 'assets/images/intro_up.png');
+    game.load.spritesheet('intro_down', 'assets/images/intro_down.png');
+    game.load.spritesheet('button_right', 'assets/images/button_right.png');    
+    game.load.spritesheet('button_left', 'assets/images/button_left.png');
+    game.load.spritesheet('header1', 'assets/images/header1.png');    
+    game.load.spritesheet('header2', 'assets/images/header2.png');
+    
+    game.load.spritesheet('main_up', 'assets/images/main_up.png');
+    game.load.spritesheet('main_down', 'assets/images/main_down.png');    
+    game.load.spritesheet('main_poster', 'assets/images/main_poster.png');
+    
+    game.load.spritesheet('icon1', 'assets/images/ICON_01.png');
+    game.load.spritesheet('icon2', 'assets/images/ICON_02.png');
+    game.load.spritesheet('icon3', 'assets/images/ICON_03.png');
+    game.load.spritesheet('icon4', 'assets/images/ICON_04.png');
+    game.load.spritesheet('icon5', 'assets/images/ICON_05.png');
+    
+    game.load.image('background','assets/images/background.png');
+    game.load.image('test_button', 'assets/images/test_button.png');
+}
+
 var intro = function(){
     this.Group = game.add.group();
     
-    this.intro = game.add.sprite(0,(h-708)/2,'intro1');
-    this.current = 1;
+    this.slide = [];
+    this.currSlide = 1;
+    for(var i = 0; i < 4; i++){
+      this.slide.push(game.add.sprite(w*i, (h-708)/2, 'intro'+(i+1).toString() ));
+    }
     
     this.left = game.add.button(0,(h-218)/2, 'button_left', this.prev, this);    
     this.right = game.add.button(w-101,(h-218)/2, 'button_right', this.next, this);    
@@ -22,7 +51,10 @@ var intro = function(){
     this.banner_down = game.add.sprite(0,h-70,'intro_down');
     
     // add things to group
-    this.Group.add(this.intro);
+    for(var i=0; i<this.slide.length; i++){
+      this.Group.add(this.slide[i]);
+    }
+
     this.Group.add(this.left);
     this.Group.add(this.right);
     this.Group.add(this.header1);
@@ -31,16 +63,16 @@ var intro = function(){
     this.Group.add(this.banner_down);
 };
 intro.prototype.next = function(){
-    if(this.current != 4) this.current += 1;
-    else this.current = 1;
-        
-    this.intro.loadTexture('intro' + this.current.toString());
+  this.currSlide ++;
+  for(var i=0; i<this.slide.length; i++){        
+    game.add.tween(this.slide[i]).to( { x: (i - this.currSlide + 1)*w }, 2000, Phaser.Easing.Quadratic.Out, true);
+  }
 };
 intro.prototype.prev = function(){
-    if(this.current != 1) this.current -= 1;
-    else this.current = 4;
-        
-    this.intro.loadTexture('intro' + this.current.toString());
+  this.currSlide --;
+  for(var i=0; i<this.slide.length; i++){
+    game.add.tween(this.slide[i]).to( { x: (i - this.currSlide + 1)*w }, 2000, Phaser.Easing.Quadratic.Out, true);
+  }
 };
 
 
@@ -77,45 +109,19 @@ var main = function(){
 
 
 var sections = [];
-
-
-function preload(){
-    game.load.spritesheet('intro1', 'assets/images/intro1.jpg');
-    game.load.spritesheet('intro2', 'assets/images/intro2.jpg');
-    game.load.spritesheet('intro3', 'assets/images/intro3.jpg');
-    game.load.spritesheet('intro4', 'assets/images/intro4.jpg');
-    game.load.spritesheet('intro_up', 'assets/images/intro_up.png');
-    game.load.spritesheet('intro_down', 'assets/images/intro_down.png');
-    game.load.spritesheet('button_right', 'assets/images/button_right.png');    
-    game.load.spritesheet('button_left', 'assets/images/button_left.png');
-    game.load.spritesheet('header1', 'assets/images/header1.png');    
-    game.load.spritesheet('header2', 'assets/images/header2.png');
-    
-    game.load.spritesheet('main_up', 'assets/images/main_up.png');
-    game.load.spritesheet('main_down', 'assets/images/main_down.png');    
-    game.load.spritesheet('main_poster', 'assets/images/main_poster.png');
-    
-    game.load.spritesheet('icon1', 'assets/images/ICON_01.png');
-    game.load.spritesheet('icon2', 'assets/images/ICON_02.png');
-    game.load.spritesheet('icon3', 'assets/images/ICON_03.png');
-    game.load.spritesheet('icon4', 'assets/images/ICON_04.png');
-    game.load.spritesheet('icon5', 'assets/images/ICON_05.png');
-    
-    game.load.image('background','assets/images/background.png');
-    game.load.image('test_button', 'assets/images/test_button.png');
-}
-
 var currPage;
 
 function changePage(){
-    if(currPage === 'intro'){
-        currPage = 'main';
-        game.world.bringToTop(sections[1].Group);
-    }
-    else{
-        currPage = 'intro';
-        game.world.bringToTop(sections[0].Group);
-    }
+  if(currPage === 'intro'){
+    currPage = 'main';
+    game.add.tween(sections[0].Group).to( { x: 0 }, 2000, Phaser.Easing.Quadratic.Out, true);
+    game.add.tween(sections[1].Group).to( { x: -1*w }, 2000, Phaser.Easing.Quadratic.Out, true);
+  }
+  else{
+    currPage = 'intro';
+    game.add.tween(sections[0].Group).to( { x: w }, 2000, Phaser.Easing.Quadratic.Out, true);
+    game.add.tween(sections[1].Group).to( { x: 0 }, 2000, Phaser.Easing.Quadratic.Out, true);
+  }
 }
 
 
@@ -127,7 +133,9 @@ function create(){
     currPage = 'intro';
     
     game.add.button(0,0,'test_button', changePage, this);
-    
+    game.world.bringToTop(sections[0].Group);
+    sections[0].Group.x = w;
+
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 }
 
